c7bec7128f50694a606e438bbafd3013769f02be
Lakshmi Gadupudi <lakshmigadupud@gmail.com>
2021-08-12 11:16:25 +0530

    test

--- vars/load.groovy
+++ vars/load.groovy
@@ -1,5 +1,5 @@
 def call (name) {
-	def scriptcontents = libraryResource "${name} "
+	def scriptcontents = libraryResource "resource/${name} "
 	writeFile file: "${name}", text: scriptcontents
 	sh "chmod a+x ./${name}"
 }
