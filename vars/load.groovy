8e13a6b434b90dfbba6de23b6dbf0d04b3e984cb
Lakshmi Gadupudi <lakshmigadupud@gmail.com>
2021-08-12 14:01:35 +0530

    test

--- vars/load.groovy
+++ vars/load.groovy
@@ -2,7 +2,7 @@ def call (name) {
 	sh "pwd"
 	sh "ls -ltr"
 	sh "ls -ltr resource/src"
-	def scriptcontents = libraryResource '/var/lib/jenkins/workspace/sharedlib/resource/src/hellotest.sh'
+	def scriptcontents = libraryResource "src/${name}"
 	writeFile file: "${name}", text: scriptcontents
 	sh "chmod a+x ./${name}"
 }
